ccf1be288eb54f63b58c88a74589f09e2d476f78
Kevin Shen <kevin_shen@hotmail.ca>
2019-06-09 14:30:08 -0400

    Created chatsocket package

---
+++ server/pkg/chatsocket/chatsocket.go
@@ -0,0 +1,51 @@
+package chatsocket
+
+import (
+    "fmt"
+    "io"
+    "log"
+    "net/http"
+
+    "github.com/gorilla/websocket"
+)
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+        // Upgrade initial GET request to a websocket
+        ws, err := upgrader.Upgrade(w, r, nil)
+        if err != nil {
+                log.Fatal(err)
+        }
+        // Make sure we close the connection when the function returns
+        defer ws.Close()
+
+        // Register our new client
+        clients[ws] = true
+
+        for {
+                var msg Message
+                // Read in a new message as JSON and map it to a Message object
+                err := ws.ReadJSON(&msg)
+                if err != nil {
+                        log.Printf("error: %v", err)
+                        delete(clients, ws)
+                        break
+                }
+                // Send the newly received message to the broadcast channel
+                broadcast <- msg
+        }
+}
+
+func handleMessages() {
+        for {
+                // Grab the next message from the broadcast channel
+                msg := <-broadcast
+                // Send it out to every client that is currently connected
+                for client := range clients {
+                        err := client.WriteJSON(msg)
+                        if err != nil {
+                                log.Printf("error: %v", err)
+                                client.Close()
+                                delete(clients, client)
+                        }
+                }
+        }
